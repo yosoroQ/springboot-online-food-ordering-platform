1a04883ecd9a48cc57b3a5bf49ed16ec5c4992e2
yosoroQ <1249508470@qq.com>
2022-09-18 20:29:27 +0800

    编写“新增员工”功能

---
+++ src/main/java/com/li/ruiji/common/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.li.ruiji.common;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+//全局异常处理
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+    //异常处理方法
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2]+"已存在";
+            return R.error(msg);
+        }
+
+
+        //账号重复报异常
+        return R.error("未知错误");
+    }
+
+}
--- src/main/java/com/li/ruiji/controller/EmployeeController.java
+++ src/main/java/com/li/ruiji/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -64,6 +65,27 @@ public class EmployeeController {
         //清理session员工id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    //新增员工
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+
+        //设置初始密码123456，进行MD5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获得当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
     }
 
 }
