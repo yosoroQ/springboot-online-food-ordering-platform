fef918794e00cbb285cd19f452762eb180bdf494
yosoroQ <1249508470@qq.com>
2022-09-26 21:34:00 +0800

    编辑员工信息

--- src/main/java/com/li/ruiji/controller/EmployeeController.java
+++ src/main/java/com/li/ruiji/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.li.ruiji.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.li.ruiji.common.R;
 import com.li.ruiji.entity.Employee;
 import com.li.ruiji.mapper.EmployeeMapper;
@@ -8,10 +10,7 @@ import com.li.ruiji.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -29,31 +28,31 @@ public class EmployeeController {
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
 //        处理逻辑如下：
-//                * 将页面提交的密码password进行md5加密处理
+//                 将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-//                * 根据页面提交的用户名username查询数据库
+//                根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
-//                * 如果没有查询到则返回登录失败结果
+//                如果没有查询到则返回登录失败结果
         if (emp == null){
             return R.error("登录失败");
         }
 
-//                * 密码比对，如果不一致则返回登录失败结果
+//                密码比对，如果不一致则返回登录失败结果
         if (!emp.getPassword().equals(password)){
             return R.error("登录失败");
         }
 
-//                * 查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+//                查看员工状态，如果为已禁用状态，则返回员工已禁用结果
         if (emp.getStatus() == 0){
             return R.error("账号已禁用");
         }
 
-//                * 登录成功，将员工id存入Session并返回登录成功结果
+//                登录成功，将员工id存入Session并返回登录成功结果
         request.getSession().setAttribute("employee",emp.getId());
 
         return R.success(emp);
@@ -86,6 +85,38 @@ public class EmployeeController {
         employeeService.save(employee);
 
         return R.success("新增员工成功");
+    }
+
+    //员工信息分页查询
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        
+        //构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+//        添加过滤条件
+        queryWrapper.like(StringUtils.isNotBlank(name),Employee::getName,name);
+//        添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        
+        //执行查询
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    //根据id修改员工信息
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+
+        return R.success("员工信息修改成功！");
+
     }
 
 }
